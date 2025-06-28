7f84d251fbbd8db0752ab042abb55b510899efc2
Tufan Ghosh <tufan.ghosh@codeclouds.co.in>
2025-06-28 12:51:18 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -11,4 +11,24 @@ $(document).ready(function () {
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
   });
+
+  $(".quotes").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1200,
+    dots: true,
+    arrows: false,
+    speed: 800,
+    cssEase: 'ease-in-out',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
