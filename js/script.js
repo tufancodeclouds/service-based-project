4006e4beab14b38f192f4c3cd7ae41eb2099ea07
Tufan Ghosh <tufan.ghosh@codeclouds.co.in>
2025-06-29 22:41:22 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -19,9 +19,6 @@ function showToast(message) {
 
 // Check login status and update navbar/bookings
 function checkLoginStatus() {
-
-  JSON.stringify(localStorage.setItem('loggedInUser', '{"name":"John Doe","email":"johndoe@example.com","password":"123"}'));
-  
   const user = JSON.parse(localStorage.getItem('loggedInUser'));
   const loginBtn = document.getElementById('loginBtn');
   const logoutBtn = document.getElementById('logoutBtn');
@@ -175,7 +172,4 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   checkLoginStatus();
   renderBookings();
-
-  $('.openBookingBtn').trigger('click');
-
 });
